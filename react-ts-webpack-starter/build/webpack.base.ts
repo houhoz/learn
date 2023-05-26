fad219f45f91436839ba2d53ea52ee9fa97b4563
hyzhao <houyazhao@gmail.com>
2023-05-26 16:00:18 +0800

    env环境配置

--- react-ts-webpack-starter/build/webpack.base.ts
+++ react-ts-webpack-starter/build/webpack.base.ts
@@ -1,7 +1,15 @@
-import { Configuration } from 'webpack'
+import { Configuration, DefinePlugin } from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-
+import * as dotenv from 'dotenv'
 const path = require('path')
+
+// 加载配置文件
+const envConfig = dotenv.config({
+  path: path.resolve(
+    __dirname,
+    '../env/.env.' + process.env.BASE_ENV
+  ),
+})
 
 const baseConfig: Configuration = {
   entry: path.join(__dirname, '../src/index.tsx'), // 入口文件
@@ -31,13 +39,27 @@ const baseConfig: Configuration = {
   // plugins
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'react-ts-webpack-starter',
+      filename: 'index.html',
       // 复制 'index.html' 文件，并自动引入打包输出的所有资源（js/css）
       template: path.join(__dirname, '../public/index.html'),
+      inject: true, // 自动注入静态资源
+      hash: true,
+      cache: false,
       // 压缩html资源
       minify: {
+        removeAttributeQuotes: true,
         collapseWhitespace: true, //去空格
         removeComments: true, // 去注释
+        minifyJS: true, // 在脚本元素和事件属性中缩小JavaScript(使用UglifyJS)
+        minifyCSS: true, // 缩小CSS样式元素和样式属性
       },
+      nodeModules: path.resolve(__dirname, '../node_modules'),
+    }),
+    new DefinePlugin({
+      'process.env': JSON.stringify(envConfig.parsed),
+      'process.env.BASE_ENV': JSON.stringify(process.env.BASE_ENV),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     }),
   ],
 }
