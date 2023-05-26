c7c25ae07b27e0f23b8418ba7df3b9a3aea435f9
hyzhao <houyazhao@gmail.com>
2023-05-26 17:25:03 +0800

    处理图片

--- react-ts-webpack-starter/build/webpack.base.ts
+++ react-ts-webpack-starter/build/webpack.base.ts
@@ -33,6 +33,7 @@ const baseConfig: Configuration = {
     path: path.join(__dirname, '../dist'), // 打包结果输出路径
     clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
     publicPath: '/', // 打包后文件的公共前缀路径
+    assetModuleFilename: 'images/[hash][ext][query]',
   },
   // loader 配置
   module: {
@@ -49,6 +50,18 @@ const baseConfig: Configuration = {
       {
         test: /\.s[ac]ss$/i,
         use: [...styleLoadersArray, 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // 匹配图片文件
+        type: 'asset', // type选择asset
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64
+          },
+        },
+        generator: {
+          filename: 'static/images/[hash][ext][query]', // 文件输出目录和命名
+        },
       },
     ],
   },
--- react-ts-webpack-starter/src/App.tsx
+++ react-ts-webpack-starter/src/App.tsx
@@ -1,5 +1,7 @@
 import '@/App.css'
-import scssStyles from './app.scss'
+import scssStyles from '@/app.scss'
+import smallImg from '@/assets/imgs/small.jpeg'
+import bigImg from '@/assets/imgs/big.jpg'
 
 function App() {
   return (
@@ -7,6 +9,10 @@ function App() {
       <h2 className='h2'>Hello world</h2>
       <div className={scssStyles['scssBox']}>
         <div className={scssStyles['box']}>scssBox</div>
+        <img src={smallImg} alt='小于10kb的图片' />
+        <img src={bigImg} alt='大于于10kb的图片' />
+        <div className={scssStyles['smallImg']}>小图片背景</div>
+        <div className={scssStyles['bigImg']}>大图片背景</div>
       </div>
     </>
   )
