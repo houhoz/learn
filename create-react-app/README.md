9168adb24424bb34e9a79656e4a9cb85476dd844
hyzhao <houyazhao@gmail.com>
2023-05-30 21:44:30 +0800

    create react app

---
+++ create-react-app/README.md
@@ -0,0 +1,11 @@
+# Getting Started with Create React App
+
+兼容低版本浏览器
+```json
+  "browserslist": [
+      ">0.2%",
+      "last 2 version",
+      "not ie <= 8"
+  ]
+```
+
---
+++ create-react-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <div>1111</div>
+  </React.StrictMode>
+)
